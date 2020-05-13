67c07c43ba673128bf532df424325f6a2725096a
ArcaneStudent <61271796+ArcaneStudent@users.noreply.github.com>
2020-05-13 17:04:27 +0300

    added bank

--- lab2/controller.py
+++ lab2/controller.py
@@ -18,6 +18,13 @@ class Controller:
             host=host, 
             port=port
             )
+        self.bank_con = psycopg2.connect(
+            database="bank_db", 
+            user="postgres", 
+            password="zxcvbn",
+            host="127.0.0.1", 
+            port="5432"
+            )        
         self.prepared = False
     
     def show_tables(self):
@@ -43,6 +50,17 @@ class Controller:
 
         for line in answer_fly:
             print("\t".join(map(lambda x: str(x).strip(), line)))
+
+        print("Bank:")
+        with self.bank_con.cursor() as bank_cursor:
+            bank_cursor.execute("SELECT * FROM bank;")
+            answer_bank = bank_cursor.fetchall()
+
+        if not self.prepared:
+            self.bank_con.rollback()
+
+        for line in answer_bank:
+            print("\t".join(map(lambda x: str(x).strip(), line)))
     
     def prepare(self, hotel_data, fly_data):
         client_name, hotel_name, arrival, departure = hotel_data
@@ -62,6 +80,29 @@ class Controller:
             )
         self.prepared = True
     
+    def bank_process(self, price=100, client_name='Anton'):
+        with self.bank_con.cursor() as bank_cursor:
+            bank_cursor.execute("SELECT amount FROM bank WHERE client_name='{}';".format(client_name))
+            answer_bank = bank_cursor.fetchone()
+
+        self.bank_con.rollback()
+        current_balance = answer_bank[0]
+
+        if current_balance >= price:
+            try:
+                with self.bank_con.cursor() as bank_cursor:
+                    bank_cursor.execute("""UPDATE bank SET amount = {}
+                                        WHERE client_name='{}';""".format(current_balance - price, client_name))
+                    self.bank_con.commit()
+                    self.commit()
+                    print("!OK") 
+            except:
+                self.bank_con.rollback()
+                self.rollback()
+                print("!ERROR happens!") 
+        
+                
+    
     def commit(self):
         self.fly_con.commit()
         self.hotel_con.commit()
@@ -72,7 +113,7 @@ class Controller:
         self.hotel_con.rollback()
         self.prepared = False
     
-    def remove_row(self, client_name):
+    def remove_row(self, client_name, amount=200):
         if not self.prepared:
             with self.hotel_con.cursor() as hotel_cursor:
                 hotel_cursor.execute("DELETE FROM hotel_booking WHERE client_name='{}';".format(client_name))
@@ -83,6 +124,11 @@ class Controller:
                 fly_cursor.execute("DELETE FROM fly_booking WHERE client_name='{}';".format(client_name))
 
             self.fly_con.commit()
+            
+            with self.bank_con.cursor() as bank_cursor:
+                bank_cursor.execute("""UPDATE bank SET amount = {}
+                                        WHERE client_name='{}';""".format(amount, client_name))
+                self.bank_con.commit()
         
 
 if __name__ == "__main__":
@@ -96,23 +142,9 @@ if __name__ == "__main__":
     fly_data = 'Anton', 'KVV 1482', 'KWW', 'KV', '6-05-2020'
     commit_controller.prepare(hotel_data, fly_data)
     
-    print("===> (c)ommit or (r)ollback?")
-    answer = ""
-    while True:
-        answer = input()
-        if answer in ["c", "r"]:
-            break
-    if answer == "r":
-        print("===> Doing rollbak...")
-        commit_controller.rollback()
-        print("===> Tables is unchanged...")
-        commit_controller.show_tables()
-    else:
-        print("===> Doing commit...")
-        commit_controller.commit()
-        print("===> Tables is changed...")
-        commit_controller.show_tables()
-        print("===> Removing new row...")
-        commit_controller.remove_row("Anton")
-        print("===> Tables is back...")
-        commit_controller.show_tables()
+    print("===> Bank operations...")
+    commit_controller.bank_process(price=100, client_name='Anton')
+    commit_controller.show_tables()
+    commit_controller.remove_row(client_name='Anton', amount=200)
+    print("===> Exiting...")
+    commit_controller.show_tables()
