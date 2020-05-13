6eb0d9eb101de199346804ad606cd308402a0e44
ArcaneStudent <61271796+ArcaneStudent@users.noreply.github.com>
2020-05-13 19:28:08 +0300

    tpt

--- lab2/controller.py
+++ lab2/controller.py
@@ -1,5 +1,6 @@
 import datetime
 import psycopg2
+from random import randint
 
 class Controller:
     def __init__(self, user, password, host="127.0.0.1", port="5432"):
@@ -24,13 +25,13 @@ class Controller:
             password="zxcvbn",
             host="127.0.0.1", 
             port="5432"
-            )        
+            )
     
     def show_tables(self):
         print("Hotel:")
-        with self.hotel_con.cursor() as hotel_cursor:
-            hotel_cursor.execute("SELECT * FROM hotel_booking;")
-            answer_hotel = hotel_cursor.fetchall()
+        hotel_cursor = self.hotel_con.cursor()
+        hotel_cursor.execute("SELECT * FROM hotel_booking;")
+        answer_hotel = hotel_cursor.fetchall()
 
         self.hotel_con.rollback()
 
@@ -39,9 +40,9 @@ class Controller:
             
 
         print("Fly:")
-        with self.fly_con.cursor() as fly_cursor:
-            fly_cursor.execute("SELECT * FROM fly_booking;")
-            answer_fly = fly_cursor.fetchall()
+        fly_cursor = self.fly_con.cursor()
+        fly_cursor.execute("SELECT * FROM fly_booking;")
+        answer_fly = fly_cursor.fetchall()
 
         self.fly_con.rollback()
 
@@ -49,9 +50,9 @@ class Controller:
             print("\t".join(map(lambda x: str(x).strip(), line)))
 
         print("Bank:")
-        with self.bank_con.cursor() as bank_cursor:
-            bank_cursor.execute("SELECT * FROM bank;")
-            answer_bank = bank_cursor.fetchall()
+        bank_cursor = self.bank_con.cursor()
+        bank_cursor.execute("SELECT * FROM bank;")
+        answer_bank = bank_cursor.fetchall()
 
         self.bank_con.rollback()
 
@@ -61,33 +62,50 @@ class Controller:
     def do_transaction(self, hotel_data, fly_data, price=100):
         try:
             client_name, hotel_name, arrival, departure = hotel_data
-            with self.hotel_con.cursor() as hotel_cursor:
-                hotel_cursor.execute("""
+            
+            seed = randint(100, 100000)
+            xid = self.hotel_con.xid(12+seed, "172312", "12312")
+            self.hotel_con.tpc_begin(xid)
+            xid = self.fly_con.xid(13+seed, "172313", "12312")
+            self.fly_con.tpc_begin(xid)
+            xid = self.bank_con.xid(14+seed, "172313", "12312")
+            self.bank_con.tpc_begin(xid)
+            
+            
+            hotel_cursor =  self.hotel_con.cursor()
+            hotel_cursor.execute("""
                 INSERT INTO hotel_booking (client_name, hotel_name, arrival, departure)
                 VALUES ('{}', '{}', '{}', '{}');
                 """.format(client_name, hotel_name, arrival, departure)
-                )
-
+            )
+            self.hotel_con.tpc_prepare()
+            
             client_name, fly_number, code_from, code_to, fly_date = fly_data
-            with self.fly_con.cursor() as fly_cursor:
-                fly_cursor.execute("""
+            fly_cursor = self.fly_con.cursor()
+            fly_cursor.execute("""
                 INSERT INTO fly_booking (client_name, fly_number, code_from, code_to, fly_date)
                 VALUES ('{}', '{}', '{}', '{}', '{}');
                 """.format(client_name, fly_number, code_from, code_to, fly_date)
-                )
+            )
+            self.fly_con.tpc_prepare()
 
-            with self.bank_con.cursor() as bank_cursor:
-                bank_cursor.execute("""
+            bank_cursor = self.bank_con.cursor()
+            bank_cursor.execute("""
                     UPDATE bank SET amount = amount - {} 
                     WHERE client_name='{}';""".format(price, client_name))
-            self.fly_con.commit()
-            self.hotel_con.commit()
-            self.bank_con.commit()
+            self.bank_con.tpc_prepare()
+            
+            input()
+            
+            self.bank_con.tpc_commit()
+            self.fly_con.tpc_commit()
+            self.hotel_con.tpc_commit()
+            
             print("!OK")
         except:
-            self.fly_con.rollback()
-            self.hotel_con.rollback()
-            self.bank_con.rollback()
+            self.bank_con.tpc_rollback()
+            self.fly_con.tpc_rollback()
+            self.hotel_con.tpc_rollback()
             print("!ERROR happens!") 
     
     def remove_row(self, client_name, amount=200):
@@ -116,7 +134,7 @@ if __name__ == "__main__":
     print("===> Doing transaction...")
     hotel_data = 'Anton', 'Kiev', '6-05-2020', '12-05-2020'
     fly_data = 'Anton', 'KVV 1482', 'KWW', 'KV', '6-05-2020'
-    commit_controller.do_transaction(hotel_data, fly_data, price=100)
+    commit_controller.do_transaction(hotel_data, fly_data, price=187)
     commit_controller.show_tables()
     commit_controller.remove_row(client_name='Anton', amount=200)
     print("===> Exiting...")
